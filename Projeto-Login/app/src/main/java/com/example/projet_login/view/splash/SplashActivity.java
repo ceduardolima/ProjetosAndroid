c4c0d32288818a8e1ba3c718a99f44ae1a438866
Eduardo <ceduardolima2000@gmail.com>
2022-10-11 11:32:10 -0300

    criando barra de progresso

--- Projeto-Login/app/src/main/java/com/example/projet_login/view/splash/SplashActivity.java
+++ Projeto-Login/app/src/main/java/com/example/projet_login/view/splash/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.example.projet_login;
+package com.example.projet_login.view.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.projet_login.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
