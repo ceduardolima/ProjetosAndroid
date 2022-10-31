c1491eda98b6163714cde6ba0624306fdfd0e803
Eduardo <ceduardolima2000@gmail.com>
2022-10-31 20:43:48 -0300

    chore: create the FirebaseFirestoreRepository

--- Projeto-Login/app/src/main/java/com/example/projet_login/repository/firebase/FirebaseFirestoreRepository.java
+++ Projeto-Login/app/src/main/java/com/example/projet_login/repository/firebase/FirebaseFirestoreRepository.java
@@ -1,2 +1,31 @@
-package com.example.projet_login.repository.firebase;public class FirebaseFirestoreRepository {
+package com.example.projet_login.repository.firebase;
+
+import android.app.Application;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.projet_login.model.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.function.Consumer;
+
+public class FirebaseFirestoreRepository {
+    private final FirebaseFirestore firestore;
+    private final Application application;
+
+    public FirebaseFirestoreRepository(Application application) {
+        firestore = FirebaseFirestore.getInstance();
+        this.application = application;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void registerUser(User user, Consumer<Boolean> block) {
+        firestore.collection("users")
+                .add(user)
+                .addOnSuccessListener((documentReference -> {
+                    block.accept(true);
+                }));
+    }
 }
